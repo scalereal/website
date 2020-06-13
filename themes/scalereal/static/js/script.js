fe92e44444b001d719663a0e1d1ad473b38177c7
ShubhamLaad <shubhamlaad666@gmail.com>
2020-06-14 00:28:26 +0530

    Create blog design

--- themes/scalereal/static/js/script.js
+++ themes/scalereal/static/js/script.js
@@ -1,44 +1,61 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
   // Homepage Projects and Testimonial sliders
-	$('.owl-one, .owl-two').owlCarousel({
-		nav: false,
+  $('.owl-one, .owl-two').owlCarousel({
+    nav: false,
     loop: true,
     margin: 0,
     dots: true,
     slideBy: 1,
     autoplay: true,
-    autoplayTimeout:15000,
+    autoplayTimeout: 15000,
     autoplaySpeed: 15000,
     autoplayHoverPause: true,
     animateOut: 'slideOutLeft',
     animateIn: 'slideInRight',
-    responsiveClass:true,
-    responsive:{
-      0:{
-        items:1,
+    responsiveClass: true,
+    responsive: {
+      0: {
+        items: 1,
       },
-      767:{
-        items:1,
+      767: {
+        items: 1,
       },
-      1199:{
-        items:1,
+      1199: {
+        items: 1,
       }
     }
-	});
+  });
 
   // Hamburger icons toggle for mobile view
   var $hamburger = $('.hamburger');
-  $hamburger.on('click', function(e) {
+  $hamburger.on('click', function (e) {
     $hamburger.toggleClass('is-active');
   });
 
   var current = location.pathname;
-  $('.navbar-nav .nav-link').each(function(index){
-      var $this = $(this);
-      // if the current path is like this link, make it active
-      if((current === '/' && index === 0 )||  ( current !== '/' && ($this.attr('href') + '/').indexOf(current) !== -1)) {
-        $this.addClass('active');
-      }
+  $('.navbar-nav .nav-link').each(function (index) {
+    var $this = $(this);
+    // if the current path is like this link, make it active
+    if ((current === '/' && index === 0) || (current !== '/' && ($this.attr('href') + '/').indexOf(current) !== -1)) {
+      $this.addClass('active');
+    }
   })
+
+  $('.js-blog-carousel').owlCarousel({
+    slideBy: 2,
+    autoplay: true,
+    responsiveClass: true,
+    touchDrag: true,
+    dots: false,
+    nav: true,
+    responsive: {
+      0: {
+        items: 2,
+      },
+      767: {
+        items: 2,
+      },
+    }
+  });
 });
