a8da82d8eaf5498227928a6e0e32abd31c694d53
Abg4real <abhyag126@gmail.com>
2021-12-07 18:09:39 +0530

    AG | Add aria labels to buttons

--- themes/scalereal/static/js/script.js
+++ themes/scalereal/static/js/script.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
-
   // Homepage Projects and Testimonial sliders
-  $('.owl-one, .owl-two').owlCarousel({
+  $(".owl-one, .owl-two").owlCarousel({
     nav: false,
     loop: true,
     margin: 0,
@@ -11,8 +10,8 @@ $(document).ready(function () {
     autoplayTimeout: 15000,
     autoplaySpeed: 15000,
     autoplayHoverPause: true,
-    animateOut: 'slideOutLeft',
-    animateIn: 'slideInRight',
+    animateOut: "slideOutLeft",
+    animateIn: "slideInRight",
     responsiveClass: true,
     responsive: {
       0: {
@@ -23,26 +22,29 @@ $(document).ready(function () {
       },
       1199: {
         items: 1,
-      }
-    }
+      },
+    },
   });
 
   // Hamburger icons toggle for mobile view
-  var $hamburger = $('.hamburger');
-  $hamburger.on('click', function (e) {
-    $hamburger.toggleClass('is-active');
+  var $hamburger = $(".hamburger");
+  $hamburger.on("click", function (e) {
+    $hamburger.toggleClass("is-active");
   });
 
   var current = location.pathname;
-  $('.navbar-nav .nav-link').each(function (index) {
+  $(".navbar-nav .nav-link").each(function (index) {
     var $this = $(this);
     // if the current path is like this link, make it active
-    if ((current === '/' && index === 0) || (current !== '/' && ($this.attr('href') + '/').indexOf(current) !== -1)) {
-      $this.addClass('active');
+    if (
+      (current === "/" && index === 0) ||
+      (current !== "/" && ($this.attr("href") + "/").indexOf(current) !== -1)
+    ) {
+      $this.addClass("active");
     }
-  })
+  });
 
-  $('.js-blog-carousel').owlCarousel({
+  $(".js-blog-carousel").owlCarousel({
     nav: true,
     loop: false,
     margin: 0,
@@ -57,37 +59,44 @@ $(document).ready(function () {
     responsive: {
       0: {
         items: 1,
-        loop: true
+        loop: true,
       },
       767: {
         items: 1,
-        loop: true
+        loop: true,
       },
       991: {
         items: 2,
-        loop: true
-      }
-    }
+        loop: true,
+      },
+    },
   });
 
-  // CURRENT
-  var senderSubMain = $('.sender-sub-main').offset();
-  var footerOffset = $('footer').offset().top;
+  const buttons = document.getElementsByClassName("owl-dot");
+  if (buttons.length !== 0) {
+    Array.from(buttons).forEach((element) => {
+      element.setAttribute("aria-label", "button");
+    });
+  }
 
-  if(senderSubMain){
+  // CURRENT
+  var senderSubMain = $(".sender-sub-main").offset();
+  var footerOffset = $("footer").offset().top;
+
+  if (senderSubMain) {
     var mainOffset = senderSubMain.top;
 
-    $(window).scroll(function(){  
-      if($(window).scrollTop() > mainOffset){
-        $('.sender-sub-main').addClass('sticky-subscription-box')
-      }else{
-        $('.sender-sub-main').removeClass('sticky-subscription-box')
+    $(window).scroll(function () {
+      if ($(window).scrollTop() > mainOffset) {
+        $(".sender-sub-main").addClass("sticky-subscription-box");
+      } else {
+        $(".sender-sub-main").removeClass("sticky-subscription-box");
       }
-    
-      if(parseInt($(window).scrollTop()) + 400 >= parseInt(footerOffset)){
-        $('.sender-sub-main').removeClass('sticky-subscription-box')
+
+      if (parseInt($(window).scrollTop()) + 400 >= parseInt(footerOffset)) {
+        $(".sender-sub-main").removeClass("sticky-subscription-box");
       }
-    })
+    });
   }
 
   function submitGR(token) {
@@ -103,7 +112,7 @@ $(document).ready(function () {
         .removeAttribute("hidden");
     }
   }
-  
+
   document.addEventListener("DOMContentLoaded", function (event) {
     document.getElementById("submit-button").setAttribute("captcha", "true");
   });
@@ -111,23 +120,21 @@ $(document).ready(function () {
 // CURRENT
 
 // INCOMING
-if($(".careers-accordion").length > 0){
-  $(document).ready(function(){
+if ($(".careers-accordion").length > 0) {
+  $(document).ready(function () {
     var url = window.location.href;
     var start = url.substr(url.indexOf("#"));
-    if(start !== '/')
-      $(start).collapse('show');
+    if (start !== "/") $(start).collapse("show");
   });
 }
 
-$(".careers-accordion").on("show.bs.collapse",function(a){
+$(".careers-accordion").on("show.bs.collapse", function (a) {
   var curr = $(a.target).attr("id");
   var url = window.location.href;
   var start = window.location.hash;
-  if(start.length > 0){
+  if (start.length > 0) {
     window.location.href = url.replace(start, "#" + curr);
-  }
-  else{
+  } else {
     window.location.href = url + "#" + curr;
   }
 });
